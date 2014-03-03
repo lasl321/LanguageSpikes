f1c774d90775e6a5e66a3447b5d3095f65eab0a5
Luis Sotomayor <LSotomayor@futuredontics.com>
2014-03-03 14:05:04 -0800

    Added some date tests

--- LanguageSpikes/DateTests.cs
+++ LanguageSpikes/DateTests.cs
@@ -6,20 +6,48 @@ namespace LanguageSpikes
     public class DateTests
     {
         [Test]
-        public void ShouldDoSomething()
+        public void ShouldHaveNowLocalDateKind()
         {
-            var now = new DateTime(2014, 3, 3, 0, 0, 0);
-            Print(now);
-            Print(now.ToLocalTime());
-
-            var nowUtc = new DateTime(2014, 3, 3, 0, 0, 0, DateTimeKind.Utc);
-            Print(nowUtc);
-            Print(nowUtc.ToLocalTime());
+            Assert.That(DateTime.Now.Kind, Is.EqualTo(DateTimeKind.Local));
         }
 
-        static void Print(DateTime dateTime)
+        [Test]
+        public void ShouldHaveLocalDateKind()
         {
-            Console.WriteLine("Kind: {0}, ToString: {1}", dateTime.Kind, dateTime);
+            var d = new DateTime(2014, 1, 1).ToLocalTime();
+            Assert.That(d.Kind, Is.EqualTo(DateTimeKind.Local));
         }
+
+        [Test]
+        public void ShouldHaveUnspecifiedDateKind()
+        {
+            var d = new DateTime(2014, 1, 1);
+            Assert.That(d.Kind, Is.EqualTo(DateTimeKind.Unspecified));
         }
+
+        [Test]
+        public void ShouldReturnFalseWhenComparingUnspecifiedAndLocalDates()
+        {
+            var dUnspecified = new DateTime(2014, 1, 1);
+            var dLocal = dUnspecified.ToLocalTime();
+
+            Assert.That(dUnspecified, Is.Not.EqualTo(dLocal));
+        }
+
+        [Test]
+        public void ShouldHaveUtcDateKind()
+        {
+            Assert.That(DateTime.UtcNow.Kind, Is.EqualTo(DateTimeKind.Utc));
+        }
+
+        [Test]
+        public void ShouldCreateUtcDateThatIsUnspecifiedToRealUtc()
+        {
+            var dUnspecified = new DateTime(2014, 1, 1);
+            var dUtc = new DateTime(dUnspecified.Ticks, DateTimeKind.Utc);
+
+            Assert.That(dUtc.Kind, Is.EqualTo(DateTimeKind.Utc));
+            Assert.That(dUtc, Is.EqualTo(dUnspecified));
+        }
+    }
 }
