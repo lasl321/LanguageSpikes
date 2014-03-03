88462d4e3c299f35ac0db6d89f1746e2047ad765
Luis Sotomayor <LSotomayor@futuredontics.com>
2014-03-03 11:44:18 -0800

    Added tests for date-related questions.

---
+++ LanguageSpikes/DateTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LanguageSpikes
+{
+    public class DateTests
+    {
+        [Test]
+        public void ShouldDoSomething()
+        {
+            var now = new DateTime(2014, 3, 3, 0, 0, 0);
+            Print(now);
+            Print(now.ToLocalTime());
+
+            var nowUtc = new DateTime(2014, 3, 3, 0, 0, 0, DateTimeKind.Utc);
+            Print(nowUtc);
+            Print(nowUtc.ToLocalTime());
+        }
+
+        static void Print(DateTime dateTime)
+        {
+            Console.WriteLine("Kind: {0}, ToString: {1}", dateTime.Kind, dateTime);
+        }
+        }
+}
